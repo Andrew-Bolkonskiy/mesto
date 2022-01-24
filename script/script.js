dceb86c5809992084e89045d1b00679ea6b25405
Andrew Bolkonskiy <a.bolkonskiy@mail.ru>
2022-01-24 15:34:11 +0300

    2.0 Учеть комментарии ревьюера

--- script/script.js
+++ script/script.js
@@ -1,11 +1,3 @@
-/* card like button */
-let likeButton = document.querySelectorAll('.card__like-btn');
-for (let i = 0; i < likeButton.length; i++){
-  likeButton[i].addEventListener('click', function(){
-    likeButton[i].classList.toggle('card__like-btn_active');
-  });
-}
-
 /* pop-up button open-close */
 let popUp = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-btn');
@@ -16,19 +8,18 @@ editButton.addEventListener('click', function() {
   nameInput.value = document.querySelector('.profile__name').textContent;
   jobInput.value = document.querySelector('.profile__occupation').textContent;
 });
+
 closeButton.addEventListener('click', function() {
   popUp.classList.remove('popup_opened');
 });
 
 // значения полей формы и кнопка сохранить
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input[name = name]');
 let jobInput = formElement.querySelector('.popup__input[name = occupation]');
 
 function formSubmitHandler (evt) {
-  evt.preventDefault(); 
-  let newName = nameInput.value;
-  let newJob = jobInput.value;
+  evt.preventDefault();
 
   document.querySelector('.profile__name').textContent = nameInput.value;
   document.querySelector('.profile__occupation').textContent = jobInput.value;
